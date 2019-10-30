0b8134a96db52b542b65347250a3a49734681a65
caiohrgm <caioh_m@hotmail.com>
2019-10-30 11:26:44 -0300

    Comentei a Classe Atividade

--- ProjetoPsquiza/src/projetolp2/usecase4/Atividade.java
+++ ProjetoPsquiza/src/projetolp2/usecase4/Atividade.java
@@ -1,18 +1,48 @@
 package projetolp2.usecase4;
-
 import java.time.Period;
 import java.util.ArrayList;
 
+/**
+ * Representação de uma Atividade Metodológica. Todoa Atividade possui um ID (códido), uma descrição da Atividade,
+ * um nível de risco, uma descrição do nível de risco e uma duração (em dias).
+ * @author caiom
+ */
 public class Atividade {
 
+    /**
+     * Código da atividade;
+     */
     private String idAtividade;
+    /**
+     * Descrição da Atividade;
+     */
     private String descricaoAtividade;
+    /**
+     * Nível de risco da Atividade;
+     */
     private String nivelRisco;
+    /**
+     * Descrição do risco;
+     */
     private String descricaoRisco;
+    /**
+     * Duração de uma Atividade;
+     */
     private Period duracao;
 
+    /**
+     * Lista dos resultados esperados.
+     */
     private ArrayList<Item> resultadosEsperados = new ArrayList<Item>();
 
+    /**
+     * Constrói uma Atividade, a partir da sua descricao, do nível de risco, da descrição do Risco e de um ID:número
+     * de identificação para formar o código.
+     * @param descricaoAtividade
+     * @param nivelRisco
+     * @param descricaoRisco
+     * @param iD
+     */
     public Atividade(String descricaoAtividade, String nivelRisco, String descricaoRisco, String iD) {
         this.descricaoAtividade = descricaoAtividade;
         this.nivelRisco = nivelRisco;
@@ -21,11 +51,19 @@ public class Atividade {
         this.duracao = Period.ofDays(8);
     }
 
+    /**
+     * Cadasta um item na lista de resultados esperados;
+     * @param descricaoItem
+     */
     public void cadastroItem(String descricaoItem) {
         Item item = new Item(descricaoItem);
         resultadosEsperados.add(item);
     }
     
+    /**
+     * Altera o status de um item de pendente para realizado;
+     * @param descricaoItem
+     */
     public void alteraStatusItem(String descricaoItem) {
         for(Item item: resultadosEsperados) {
             if(item.getDescricao().equals(descricaoItem)) {
@@ -34,6 +72,10 @@ public class Atividade {
         }
     }
     
+    /**
+     * Conta quantos itens pedentens existem na lista;
+     * @return
+     */
     public int totalItemsPendentes() {
         int count = 0;
         for (int i = 0; i < resultadosEsperados.size(); i++) {
@@ -44,6 +86,10 @@ public class Atividade {
         return count;
     }
     
+    /**
+     * Conta quantos itens realizados existem na lista;
+     * @return
+     */
     public int totalItemsRealizados() {
         int count = 0;
         for (int i = 0; i < resultadosEsperados.size(); i++) {
@@ -54,6 +100,10 @@ public class Atividade {
         return count;
     }
     
+    /**
+     * Retorna o as informações de todos os items Realizaods;
+     * @return
+     */
     private String getStringRealizados() {
         String realizados = "";
         for (int i = 0; i < resultadosEsperados.size(); i++) {
@@ -68,6 +118,10 @@ public class Atividade {
         return realizados;
     }
     
+    /**
+     * Retorna as informações de todos os itens pendentes;
+     * @return
+     */
     private String getStringPendentes() {
         String pendentes = "";
         for (int i = 0; i < resultadosEsperados.size(); i++) {
