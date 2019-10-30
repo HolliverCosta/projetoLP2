fc4f9557d618f5e1de9eb22b8a3b5e6a6903b6c8
caiohrgm <caioh_m@hotmail.com>
2019-10-30 11:27:01 -0300

    Comentei a Classe Item

--- ProjetoPsquiza/src/projetolp2/usecase4/Item.java
+++ ProjetoPsquiza/src/projetolp2/usecase4/Item.java
@@ -1,23 +1,57 @@
 package projetolp2.usecase4;
 
+/**
+ * Representação de um Item. Todo Item possui uma descrição e um status, que é
+ * definido como "PENDENTE" inicialmente;
+ * 
+ * @author caiom
+ *
+ */
 public class Item {
 
+    /**
+     * Descrição do item;
+     */
     private String descricao;
+    /**
+     * Status do item;
+     */
     private String status;
 
+    /**
+     * COnstrói um Item a partir da sua descrição. Define o atributo status para
+     * "PENDENTE";
+     * 
+     * @param descricao
+     */
     public Item(String descricao) {
         this.descricao = descricao;
         this.status = "PENDENTE";
     }
 
+    /**
+     * Retorna a descrição do Item;
+     * 
+     * @return
+     */
     public String getDescricao() {
         return descricao;
     }
 
+    /**
+     * Retorna o status do Item;
+     * 
+     * @return
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Define o status do Item;
+     * 
+     * @param status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
