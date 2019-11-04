179b7e855d1a58165fa391d1fa194855deb47833
Danilo César Ribeiro Garcia de Medeiros <danilocrgm@gmail.com>
2019-11-04 17:25:48 -0300

    gerador de comparador para ordenaçao

---
+++ ProjetoPsquiza/src/projetolp2/pesquisa/GeraComparador.java
@@ -0,0 +1,42 @@
+package projetolp2.pesquisa;
+
+import java.util.Comparator;
+
+public class GeraComparador {
+    
+    public static Comparator<Pesquisa> geraComparador(String ordem){
+        Comparator<Pesquisa> comparador;
+        if(ordem.equals("PROBLEMA")) {
+            comparador = new Comparator<Pesquisa>() {
+                @Override
+                public int compare(Pesquisa p0, Pesquisa p1) {
+                    return p1.getProblema().compareTo(p0.getProblema());
+                }            
+            };
+        }
+        else if(ordem.equals("OBJETIVOS")) {
+            comparador = new Comparator<Pesquisa>() {
+                @Override
+                public int compare(Pesquisa p0, Pesquisa p1) {
+                    if(p0.getObjetivos().size() == p1.getObjetivos().size()) {
+                        return 1;
+                    }else if(p0.getObjetivos().size() > p1.getObjetivos().size()) {
+                        return -1;
+                    }
+                    else {
+                        return 1;
+                    }
+                }
+            };
+        }
+        else {
+            comparador = new Comparator<Pesquisa>() {
+                @Override
+                public int compare(Pesquisa p0, Pesquisa p1) {
+                   return p1.getCodigo().compareTo(p0.getCodigo());
+                }
+            };
+        }
+        return comparador;
+    }
+}
