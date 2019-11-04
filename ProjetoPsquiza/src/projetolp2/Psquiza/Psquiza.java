ee19c63e751bcd62dd2d7c1ca761a92dacc43f4b
Danilo César Ribeiro Garcia de Medeiros <danilocrgm@gmail.com>
2019-11-04 17:26:59 -0300

    us5 implementada

--- ProjetoPsquiza/src/projetolp2/Psquiza/Psquiza.java
+++ ProjetoPsquiza/src/projetolp2/Psquiza/Psquiza.java
@@ -4,6 +4,7 @@ import projetolp2.atividades.ControllerAtividade;
 import projetolp2.pesquisa.ControllerPesquisa;
 import projetolp2.pesquisador.ControllerPesquisador;
 import projetolp2.po.ControllerPO;
+import projetolp2.po.ValidaCampos;
 
 public class Psquiza {
 	private ControllerAtividade controllerAtividade;
@@ -18,16 +19,48 @@ public class Psquiza {
 		this.controllerPO = new ControllerPO();				
 	}
 	public ControllerAtividade getControllerAtividade() {
-		return controllerAtividade;
+		return this.controllerAtividade;
 	}
 	
 	public ControllerPesquisa getControllerPesquisa() {
-		return controllerPesquisa;
+		return this.controllerPesquisa;
 	}
 	public ControllerPesquisador getControllerPesquisador() {
-		return controllerPesquisador;
+		return this.controllerPesquisador;
 	}
 	public ControllerPO getControllerPO() {
-		return controllerPO;
+		return this.controllerPO;
+	}
+	
+	public boolean associaProblema(String idPesquisa, String idProblema) {
+	       ValidaCampos.validaCamposString(new String[] {idPesquisa, idProblema},
+	                new String[] {"idPesquisa", "idProblema"});
+	    if(!this.controllerPO.existe(idProblema)) throw new IllegalArgumentException("Problema nao encontrado");
+	    return this.controllerPesquisa.associaProblema(idPesquisa, idProblema);
+	}
+	
+	public boolean desassociaProblema(String idPesquisa, String idProblema) {
+	       ValidaCampos.validaCamposString(new String[] {idPesquisa, idProblema},
+	                new String[] {"idPesquisa", "idProblema"});
+	    if(!this.controllerPO.existe(idProblema)) throw new IllegalArgumentException("Problema nao encontrado");
+	    return this.controllerPesquisa.desassociaProblema(idPesquisa, idProblema);
+	}
+	
+	public boolean associaObjetivo(String idPesquisa, String idObjetivo) {
+	    ValidaCampos.validaCamposString(new String[] {idPesquisa, idObjetivo},
+                new String[] {"idPesquisa", "idObjetivo"});
+	    if(!this.controllerPO.existe(idObjetivo)) throw new IllegalArgumentException("Objetivo nao encontrado");
+	    return this.controllerPesquisa.associaObjetivo(idPesquisa, idObjetivo);
+	}
+	
+	public boolean desassociaObjetivo(String idPesquisa, String idObjetivo) {
+	    ValidaCampos.validaCamposString(new String[] {idPesquisa, idObjetivo},
+                new String[] {"idPesquisa", "idObjetivo"});
+	    if(!this.controllerPO.existe(idObjetivo)) throw new IllegalArgumentException("Objetivo nao encontrado");
+	    return this.controllerPesquisa.desassociaObjetivo(idPesquisa, idObjetivo);
+	}
+	
+	public String listaPesquisas(String ordem) {
+	    return this.controllerPesquisa.listaPesquisas(ordem);
 	}
 }
