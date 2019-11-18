c89462c804d052d7f23777fb5042e158a96681a9
Danilo César Medeiros <danilocrgm@gmail.com>
2019-11-18 09:50:04 -0300

    implementado metodos para salvar e carregar o estado do sistema

--- ProjetoPsquiza/src/projetolp2/Psquiza/Psquiza.java
+++ ProjetoPsquiza/src/projetolp2/Psquiza/Psquiza.java
@@ -1,5 +1,10 @@
 package projetolp2.Psquiza;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import projetolp2.atividades.ControllerAtividade;
 import projetolp2.busca.ControllerBusca;
 import projetolp2.misc.ValidaCampos;
@@ -9,12 +14,30 @@ import projetolp2.pesquisador.ControllerPesquisador;
 import projetolp2.po.ControllerPO;
 
 public class Psquiza {
+    /**
+     * 
+     */
 	private ControllerAtividade controllerAtividade;
+	/**
+	 * 
+	 */
 	private ControllerPesquisa controllerPesquisa;
+	/**
+	 * 
+	 */
 	private ControllerPesquisador controllerPesquisador;
+	/**
+	 * 
+	 */
 	private ControllerPO controllerPO;
+	/**
+	 * 
+	 */
 	private ControllerBusca controllerBusca;
 	private Validacao validacao;
+	/**
+	 * 
+	 */
 	public Psquiza() {	
 		this.controllerAtividade = new ControllerAtividade();
 		this.controllerPesquisa = new ControllerPesquisa();
@@ -23,6 +46,7 @@ public class Psquiza {
 		this.controllerBusca = new ControllerBusca();
 		this.validacao = new Validacao();
 	}
+	
 	public ControllerAtividade getControllerAtividade() {
 		return this.controllerAtividade;
 	}
@@ -30,12 +54,14 @@ public class Psquiza {
 	public ControllerPesquisa getControllerPesquisa() {
 		return this.controllerPesquisa;
 	}
+	
 	public ControllerPesquisador getControllerPesquisador() {
 		return this.controllerPesquisador;
 	}
 	public ControllerPO getControllerPO() {
 		return this.controllerPO;
 	}
+	
 	public ControllerBusca getControllerBusca() {
 		return controllerBusca;
 	}
@@ -47,11 +73,11 @@ public class Psquiza {
 	    return this.controllerPesquisa.associaProblema(idPesquisa, idProblema);
 	}
 	
-	public boolean desassociaProblema(String idPesquisa, String idProblema) {
-	       ValidaCampos.validaCamposString(new String[] {idPesquisa, idProblema},
-	                new String[] {"idPesquisa", "idProblema"});
-	    if(!this.controllerPO.existe(idProblema)) throw new IllegalArgumentException("Problema nao encontrado");
-	    return this.controllerPesquisa.desassociaProblema(idPesquisa, idProblema);
+	public boolean desassociaProblema(String idPesquisa) {
+	       ValidaCampos.validaCamposString(new String[] {idPesquisa},
+	                new String[] {"idPesquisa"});
+	    //if(!this.controllerPO.existe(idProblema)) throw new IllegalArgumentException("Problema nao encontrado");
+	    return this.controllerPesquisa.desassociaProblema(idPesquisa);
 	}
 	
 	public boolean associaObjetivo(String idPesquisa, String idObjetivo) {
@@ -111,5 +137,62 @@ public class Psquiza {
 	public String proximaAtividade(String codigoPesquisa) {
 		validacao.validaString(codigoPesquisa, "Pesquisa nao pode ser nula ou vazia.");
 		return controllerPesquisa.proximaAtividade(codigoPesquisa);
+	}
+	
+	public void salva() {
+	    String dir = "persistencia";
+	    try {
+	        FileOutputStream fout1 = new FileOutputStream(dir + "/atividades.ser"); 
+	        FileOutputStream fout2 = new FileOutputStream(dir + "/pesquisas.ser"); 
+	        FileOutputStream fout3 = new FileOutputStream(dir + "/pesquisadores.ser"); 
+	        FileOutputStream fout4 = new FileOutputStream(dir + "/problemasEobjetivos.ser"); 
+	        
+	        ObjectOutputStream oos1 = new ObjectOutputStream(fout1);
+	        ObjectOutputStream oos2 = new ObjectOutputStream(fout2);
+	        ObjectOutputStream oos3 = new ObjectOutputStream(fout3);
+	        ObjectOutputStream oos4 = new ObjectOutputStream(fout4);
+	        
+	        oos1.writeObject(this.controllerAtividade);
+	        oos2.writeObject(this.controllerPesquisa);
+	        oos3.writeObject(this.controllerPesquisador);
+	        oos4.writeObject(this.controllerPO);
+	        
+	        oos1.close();
+	        oos2.close();
+	        oos3.close();
+	        oos4.close();
+	        
+	    }
+	    catch(Exception ex) {
+	        ex.printStackTrace();
+	    }
+	}
+	
+	public void carrega() {
+	    String dir = "persistencia";
+	    try {
+	        FileInputStream fin1 = new FileInputStream(dir + "/atividades.ser");
+	        FileInputStream fin2 = new FileInputStream(dir + "/pesquisas.ser");
+	        FileInputStream fin3 = new FileInputStream(dir + "/pesquisadores.ser");
+	        FileInputStream fin4 = new FileInputStream(dir + "/problemasEobjetivos.ser");
+	        
+	        ObjectInputStream ois1 = new ObjectInputStream(fin1);
+	        ObjectInputStream ois2 = new ObjectInputStream(fin2);
+	        ObjectInputStream ois3 = new ObjectInputStream(fin3);
+	        ObjectInputStream ois4 = new ObjectInputStream(fin4);
+	        
+	        this.controllerAtividade = (ControllerAtividade) ois1.readObject();
+	        this.controllerPesquisa = (ControllerPesquisa) ois2.readObject();
+	        this.controllerPesquisador = (ControllerPesquisador) ois3.readObject();
+	        this.controllerPO = (ControllerPO) ois4.readObject();
+	        
+	        ois1.close();
+	        ois2.close();
+	        ois3.close();
+	        ois4.close();
+	    }
+	    catch(Exception ex) {
+	        ex.printStackTrace();
+	    }
 	}
 }
